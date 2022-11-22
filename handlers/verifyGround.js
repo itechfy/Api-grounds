1281f851b3629033f11413336a09bf89d0ebacb8
Hamza Shah - TechFy <72313552+itechfy@users.noreply.github.com>
2022-11-23 02:11:52 +0500

    Add files via upload

---
+++ handlers/verifyGround.js
@@ -0,0 +1,17 @@
+const { db } = require("../utils/admin");
+// const { Cricket, Football } = require("../data/data.ground");
+// const { Owners } = require("../data/data.owner");
+exports.setVerified = async (req, res) => {
+  const { id, type, city } = req.query;
+  try {
+    let paramsString = `${city}.${id}.verified`; // DOT Notation to update nested objects
+    const obj = {
+      [paramsString]: true,
+    };
+    db.doc(`grounds/${type}`).update(obj);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ general: "Something went wrong, please try again" });
+  }
+};
