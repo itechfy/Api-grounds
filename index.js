49281d23d6117277ac4bb2cefc67eac4115197c5
Hamza Shah - TechFy <72313552+itechfy@users.noreply.github.com>
2022-11-23 02:12:27 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { grounds } = require("./handlers/grounds");
 const { owner } = require("./handlers/ground-owner");
 const { owners } = require("./handlers/all-owners");
 const { groundAndOwner } = require("./handlers/ground-and-owner");
+const { setVerified } = require("./handlers/verifyGround");
 
 var app = express();
 const PORT = process.env.PORT || 5050;
@@ -28,6 +29,8 @@ app.get("/owner", owners);
 
 app.get("/ground-and-owner", groundAndOwner);
 
+// example.com/verify/ground ?id=1&type=cricket&city=islamabad            //id of ground to verify
+app.patch("/verify/ground*", setVerified);
 
 app.listen(PORT, function () {
   console.log(`Demo project at: ${PORT}!`);
